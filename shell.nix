6235750163d16de93ce01364d0773053c397e3a8
Yan Lin <linyanwd213@gmail.com>
2025-06-18 00:51:12 +0000

    Quartz sync: Jun 18, 2025, 12:51 AM

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> {}, serve ? false, sync ? true }:
+{ pkgs ? import <nixpkgs> {}, serve ? false, sync ? true, push ? false }:
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
@@ -9,10 +9,13 @@ pkgs.mkShell {
 
   shellHook = ''
       rsync -avP --delete ~/Obsidian/Blog/ ./content/ --exclude ".*"
-      ${if serve then '' npx quartz build --serve && exit'' else ''''}
+      ${if serve then '' npx quartz build --serve'' else ''''}
       ${if sync then ''
-        npx quartz build && npx quartz sync && rsync -avP --delete ./{public,compose.yml,nginx.conf} personal-vps:/root/blog/
-        exit
+        npx quartz build && rsync -avP --delete ./{public,compose.yml,nginx.conf} personal-vps:/root/blog/
       '' else ''''}
+      ${if push then ''
+        npx quartz build && npx quartz sync
+      '' else ''''}
+      exit
   '';
 }

b06668edafbc4adc8ceef5b4e4d28f4e0c7a6248
Yan Lin <linyanwd213@gmail.com>
2025-07-26 07:42:26 +0000

    Quartz sync: Jul 26, 2025, 7:42 AM

--- tmux.sh
+++ tmux.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 SESSION_NAME="blog"
+WORK_DIR="/Users/yanlin/Documents/Projects/personal-blog"
 
 if tmux has-session -t $SESSION_NAME 2>/dev/null; then
     tmux attach-session -t $SESSION_NAME
     exit 0
 fi
 
-tmux new-session -d -s $SESSION_NAME -c "/Users/yanlin/Documents/Projects/personal-blog"
+tmux new-session -d -s $SESSION_NAME -c "$WORK_DIR"
 tmux rename-window -t $SESSION_NAME:1 "nvim"
 tmux send-keys -t $SESSION_NAME:1 "nvim" C-m
 tmux new-window -t $SESSION_NAME:2 -n "ai"
 tmux send-keys -t $SESSION_NAME:2 "claude -r" C-m
+tmux split-window -t $SESSION_NAME:2 -h
+tmux split-window -t $SESSION_NAME:2.2 -v
+tmux select-pane -t $SESSION_NAME:2.1
 tmux new-window -t $SESSION_NAME:3 -n "bash"
 
 tmux select-window -t $SESSION_NAME:1
